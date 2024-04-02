43cf65b246c212c790131658eb8cb3dec099a286
Rafael Zimmer <rzimmerdev@gmail.com>
2024-04-02 02:19:03 -0300

    Added host-config

--- dxforge/clusters/node/instance.py
+++ dxforge/clusters/node/instance.py
@@ -30,12 +30,13 @@ class Instance:
         return None
 
     def create(self, docker_client: DockerClient) -> Container:
-        # TODO: --add-host=host.docker.internal:host-gateway
         self._container = docker_client.containers.create(
             image=self.data.build.tag,
             ports=self.data.run.ports,
             network=self.data.run.network if self.data.run.network != "host" else None,
             detach=True,
+            host_config=docker_client.create_host_config(extra_hosts={'host.docker.internal': 'host-gateway'})
+
         )
         return self._container
 
